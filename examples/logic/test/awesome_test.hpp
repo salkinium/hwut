28b5a62e3a4d247f4a8d15379cb7785a118c796c
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-08-01 13:44:44 +0200

    [examples] correct header

--- examples/logic/test/awesome_test.hpp
+++ examples/logic/test/awesome_test.hpp
@@ -1,18 +1,22 @@
-/* Copyright (c) 2015, Roboterclub Aachen e.V.
- * This file is part of roboterclubaachen/hwut.
+/*
+ * awesome_test.hpp
  *
- * roboterclubaachen/hwut is free software: you can redistribute it and/or modify
+ * Copyright (c) 2015, Roboterclub Aachen e.V.
+ *
+ * This file is part of hwut.
+ *
+ * hwut is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * roboterclubaachen/hwut is distributed in the hope that it will be useful,
+ * hwut is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with roboterclubaachen/hwut.  If not, see <http://www.gnu.org/licenses/>.
+ * along with hwut.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <unittest/testsuite.hpp>
